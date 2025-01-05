8fd7c4c9696b2d3183383193440da4c69dd2a1d0
Yurkin PI-31 <jurkinmisha1@gmail.com>
2025-01-05 19:31:58 +0700

    Накидал основу для главного меню

--- MyForm.h
+++ MyForm.h
@@ -37,6 +37,11 @@ namespace kurs2 {
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ buttonplay;
 	private: System::Windows::Forms::Button^ buttonrule;
+	private: System::Windows::Forms::Button^ buttonexit;
+
+
+
+
 	protected:
 
 
@@ -55,6 +60,7 @@ namespace kurs2 {
 		{
 			this->buttonplay = (gcnew System::Windows::Forms::Button());
 			this->buttonrule = (gcnew System::Windows::Forms::Button());
+			this->buttonexit = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// buttonplay
@@ -65,7 +71,7 @@ namespace kurs2 {
 			this->buttonplay->Location = System::Drawing::Point(215, 271);
 			this->buttonplay->Name = L"buttonplay";
 			this->buttonplay->Size = System::Drawing::Size(282, 100);
-			this->buttonplay->TabIndex = 0;
+			this->buttonplay->TabIndex = 1;
 			this->buttonplay->Text = L"ÈÃÐÀÒÜ";
 			this->buttonplay->UseVisualStyleBackColor = true;
 			this->buttonplay->Click += gcnew System::EventHandler(this, &MyForm::buttonplay_Click);
@@ -78,28 +84,43 @@ namespace kurs2 {
 			this->buttonrule->Location = System::Drawing::Point(727, 271);
 			this->buttonrule->Name = L"buttonrule";
 			this->buttonrule->Size = System::Drawing::Size(282, 100);
-			this->buttonrule->TabIndex = 1;
+			this->buttonrule->TabIndex = 2;
 			this->buttonrule->Text = L"ÏÐÀÂÈËÀ";
 			this->buttonrule->UseVisualStyleBackColor = true;
 			this->buttonrule->Click += gcnew System::EventHandler(this, &MyForm::buttonrule_Click);
+			// 
+			// buttonexit
+			// 
+			this->buttonexit->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->buttonexit->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->buttonexit->Location = System::Drawing::Point(1137, 12);
+			this->buttonexit->Name = L"buttonexit";
+			this->buttonexit->Size = System::Drawing::Size(121, 47);
+			this->buttonexit->TabIndex = 3;
+			this->buttonexit->Text = L"Âûõîä";
+			this->buttonexit->UseVisualStyleBackColor = true;
+			this->buttonexit->Click += gcnew System::EventHandler(this, &MyForm::buttonexit_Click);
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(1293, 699);
+			this->Controls->Add(this->buttonexit);
 			this->Controls->Add(this->buttonrule);
 			this->Controls->Add(this->buttonplay);
 			this->Location = System::Drawing::Point(367, 284);
 			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"Áëýêäæåê";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+
 	}
 	
 	private: System::Void buttonrule_Click(System::Object^ sender, System::EventArgs^ e) {
@@ -107,5 +128,9 @@ namespace kurs2 {
 	}
 	private: System::Void buttonplay_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	
+private: System::Void buttonexit_Click(System::Object^ sender, System::EventArgs^ e) {
+	Application::Exit();
+}
+};
 }
